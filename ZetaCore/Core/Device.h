585569865fc94e6d02ac8eb614a43f7517853b52
alipbcs <78628849+alipbcs@users.noreply.github.com>
2023-03-06 15:39:26 -0800

    Update Agility SDK to version 1.608.3

--- ZetaCore/Core/Device.h
+++ ZetaCore/Core/Device.h
@@ -2,8 +2,8 @@
 
 #include "../App/ZetaRay.h"
 #include "../Win32/Win32.h"
-#include <D3D12/1.606.3/d3d12.h>
-#include <D3D12/1.606.3/dxgiformat.h>
+#include <D3D12/1.608.3/d3d12.h>
+#include <D3D12/1.608.3/dxgiformat.h>
 #include <dxgi1_6.h>
 
 #ifdef _DEBUG
